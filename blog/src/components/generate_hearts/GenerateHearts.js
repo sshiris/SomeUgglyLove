87e30debe500e0f1fcaedbdde7de8716706bafe6
Iris <148431646+iris915520@users.noreply.github.com>
2025-04-06 12:17:36 +0300

    draw partical

--- blog/src/components/generate_hearts/GenerateHearts.js
+++ blog/src/components/generate_hearts/GenerateHearts.js
@@ -1,7 +1,52 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-const GenerateHearts = () => {
-  return <div></div>;
+const GenerateHearts = ({ canvasRef }) => {
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+    const mouse = {
+      x: undefined,
+      y: undefined,
+    };
+    class Particle {
+      constructor() {
+        this.x = mouse.x;
+        this.y = mouse.y;
+        this.radius = Math.random() * 80 + 1;
+        this.speedX = Math.random() * 3 - 1.5;
+        this.speedY = Math.random() * 3 - 1.5;
+        this.color = `hsl(${Math.random() * 360}, 100%, 50%)`;
+      }
+      update() {
+        this.x += this.speedX;
+        this.y += this.speedY;
+        if (this.radius > 0.2) {
+          this.radius -= 0.1;
+        }
+      }
+
+      draw() {
+        this.update();
+        ctx.fillStyle = this.color;
+        ctx.beginPath();
+
+        ctx.moveTo(this.x, this.y);
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI);
+        ctx.arc(this.x, this.y, this.radius / 2, Math.PI, 2 * Math.PI);
+        ctx.arc(
+          this.x - this.radius,
+          this.y,
+          this.radius / 2,
+          Math.PI,
+          2 * Math.PI
+        );
+
+        ctx.closePath();
+        ctx.fill();
+      }
+    }
+  });
+  return null;
 };
 
 export default GenerateHearts;
