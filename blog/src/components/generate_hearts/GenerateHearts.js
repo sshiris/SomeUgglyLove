1c874f0bc36f804d764de6f5a500399acd7fb99d
Iris <148431646+iris915520@users.noreply.github.com>
2025-04-06 15:59:10 +0300

    class partical

--- blog/src/components/generate_hearts/GenerateHearts.js
+++ blog/src/components/generate_hearts/GenerateHearts.js
@@ -1,6 +1,8 @@
-import React, { useEffect } from "react";
+import { useEffect, useRef } from "react";
+import HeartMovements from "../heartMovements/HeartMovements";
 
 const GenerateHearts = ({ canvasRef }) => {
+  const particlesArray = useRef([]);
   useEffect(() => {
     const canvas = canvasRef.current;
     const ctx = canvas.getContext("2d");
@@ -8,14 +10,31 @@ const GenerateHearts = ({ canvasRef }) => {
       x: undefined,
       y: undefined,
     };
+
+    canvas.addEventListener("click", (event) => {
+      mouse.x = event.x;
+      mouse.y = event.y;
+      for (let i = 0; i < 3; i++) {
+        particlesArray.current.push(new Particle());
+      }
+    });
+    canvas.addEventListener("mousemove", (event) => {
+      mouse.x = event.x;
+      mouse.y = event.y;
+      for (let i = 0; i < 2; i++) {
+        particlesArray.current.push(new Particle());
+      }
+    });
+
     class Particle {
       constructor() {
         this.x = mouse.x;
         this.y = mouse.y;
         this.radius = Math.random() * 80 + 1;
-        this.speedX = Math.random() * 3 - 1.5;
-        this.speedY = Math.random() * 3 - 1.5;
-        this.color = `hsl(${Math.random() * 360}, 100%, 50%)`;
+        this.speedX = Math.random() * 2 - 1;
+        this.speedY = Math.random() * 2 - 1;
+        const hue = Math.random() * 30 + 260; //and a purple
+        this.color = `hsl(${hue}, 100%, 50%)`;
       }
       update() {
         this.x += this.speedX;
@@ -31,7 +50,7 @@ const GenerateHearts = ({ canvasRef }) => {
         ctx.beginPath();
 
         ctx.moveTo(this.x, this.y);
-        ctx.arc(this.x, this.y, this.radius, 0, Math.PI);
+        ctx.arc(this.x - this.radius / 2, this.y, this.radius, 0, Math.PI);
         ctx.arc(this.x, this.y, this.radius / 2, Math.PI, 2 * Math.PI);
         ctx.arc(
           this.x - this.radius,
@@ -46,7 +65,14 @@ const GenerateHearts = ({ canvasRef }) => {
       }
     }
   });
-  return null;
+  return (
+    <div>
+      <HeartMovements
+        canvasRef={canvasRef}
+        particlesArray={particlesArray.current}
+      />
+    </div>
+  );
 };
 
 export default GenerateHearts;
